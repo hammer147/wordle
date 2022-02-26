14fd1ddc099515f5c6ef90810541aa76324941d1
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2022-02-26 13:00:54 +0100

    add logic to pick a word

--- pages/api/word.ts
+++ pages/api/word.ts
@@ -1,17 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+type Data = { word: string }
 
-const wordsDb = ['proxy','space','table','games']
-const getIndexFromDate = () => 0 // TODO
+const wordsDb = ['toxic', 'space', 'table', 'games']
 
-type Data = {
-  word: string
-}
+const millisecondsToDays = (milliseconds: number) => Math.floor(milliseconds / 1000 / 60 / 60 / 24)
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ word: wordsDb[getIndexFromDate()] })
+const dayStart = millisecondsToDays(new Date('2022-02-24').valueOf())
+const dayIncomingRequest = millisecondsToDays(new Date().valueOf())
+const daysSinceStart = dayIncomingRequest - dayStart
+const idx = daysSinceStart % wordsDb.length 
+const word = wordsDb[idx]
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ word })
 }
