2151164fdb4c55579c9a3a2e3ca10dd358aa6762
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2022-03-12 10:03:28 +0100

    improved styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
 import type { NextPage } from 'next'
 import Wordle from '../components/wordle/wordle'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <Wordle />
-    </div>
-  )
+  return  <Wordle />
 }
 
 export default Home
